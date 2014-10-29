01bd31a6d72a644b0b58a87bd12e59d6e07287c5
Alexey Samoylov <alexey.samoylov@gmail.com>
2014-10-29 17:28:53 +0700

    empty files

--- src/Behavior.php
+++ src/Behavior.php
@@ -1,1 +1,12 @@
 <?php
+/**
+ * @author Alexey Samoylov <alexey.samoylov@gmail.com>
+ * @author Valentin Konusov <rlng-krsk@yandex.ru>
+ */
+
+namespace yiidreamteam\wizard;
+
+class Behavior extends \yii\base\Behavior
+{
+
+}
